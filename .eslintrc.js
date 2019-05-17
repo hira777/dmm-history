5597536d834d537e80efdf1fb7477b78ece9a766
soarflat <soarflat777@gmail.com>
2019-05-18 02:11:50 +0900

    chore: .eslintrc.yaml -> .eslintrc.js 設定も少し変更した

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2017
+  },
+  extends: ['plugin:vue/recommended', 'plugin:prettier/recommended'],
+  plugins: ['vue'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        bracketSpacing: true
+      }
+    ]
+  },
+  globals: {
+    chrome: true
+  }
+};
