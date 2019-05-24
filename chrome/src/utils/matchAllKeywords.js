509a2c853277415d004f2cc40031b90eab167c37
soarflat <soarflat777@gmail.com>
2019-05-24 21:45:01 +0900

    refactor: キーワード検索ロジックをリファクタリングした。テストも追加した。

--- chrome/src/utils/matchAllKeywords.js
+++ chrome/src/utils/matchAllKeywords.js
@@ -1,6 +1,6 @@
 /**
  * 全てのキーワードが検索対象に含まれているかどうか（and検索）
- * @param keywords {Array} キーワード
+ * @param keywords {String[]} キーワード
  * @param target {String} 検索対象
  * @return {boolean}
  */
@@ -8,15 +8,10 @@ const matchAllKeywords = ({ keywords, target }) => {
   if (keywords === undefined || keywords.length === 0 || keywords[0] === '')
     return false;
 
-  let matchCount = 0;
-
-  for (let i = 0; i < keywords.length; i += 1) {
-    const re = new RegExp(`.*${keywords[i]}.*`, 'i');
-    if (re.test(target)) matchCount += 1;
-    if (matchCount !== i + 1) break;
-  }
-
-  return matchCount === keywords.length;
+  return keywords.every(keyword => {
+    const re = new RegExp(`.*${keyword}.*`, 'i');
+    return re.test(target);
+  });
 };
 
 export default matchAllKeywords;
