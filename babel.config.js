f212dbacac4f71e3431c98e334720dfa25b33d21
soarflat <soarflat777@gmail.com>
2019-11-23 22:27:37 +0900

    test: 最小の TS 用のテスト環境を構築(#17)
    - `.ts`ファイルのテストが実行できるように設定を更新
    - Jest の型定義ファイルを追加

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 // babel.config.js
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]]
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript'
+  ]
 };
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/chrome/src/$1'
   },
-  moduleFileExtensions: ['js', 'vue', 'json']
+  moduleFileExtensions: ['js', 'ts', 'vue', 'json']
 };
