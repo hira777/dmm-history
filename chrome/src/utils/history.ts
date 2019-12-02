50020767c7a54b30bf1632869cd5ccaed320eb5a
soarflat <soarflat777@gmail.com>
2019-12-03 03:10:43 +0900

    refactor: 関数をアロー関数に統一(#19)

--- chrome/src/utils/history.ts
+++ chrome/src/utils/history.ts
@@ -1,15 +1,15 @@
 import { ChromeStorageSchema } from '@/models/chromeStorageSchema';
 import { History, Histories } from '@/models/history';
 
-export function add(histories: Histories, newHistory: History): Histories {
+export const add = (histories: Histories, newHistory: History): Histories => {
   return [newHistory, ...histories.filter(({ id }) => id !== newHistory.id)];
-}
+};
 
-export function get(obj: ChromeStorageSchema): Histories {
+export const get = (obj: ChromeStorageSchema): Histories => {
   return obj.dmmHistory && obj.dmmHistory.histories
     ? obj.dmmHistory.histories
     : [];
-}
+};
 
 export default {
   add,
