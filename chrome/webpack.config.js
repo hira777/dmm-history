882ff767cc3f68148e474f6fbab3785bff1677d1
soarflat <soarflat777@gmail.com>
2019-12-18 20:59:46 +0900

    chore: 開発ビルドの時だけソースマップ（cheap-module-eval-source-map）を生成するようにした

--- chrome/webpack.config.js
+++ chrome/webpack.config.js
@@ -4,68 +4,71 @@ const TerserPlugin = require('terser-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 /* eslint-enable @typescript-eslint/no-var-requires */
 
-module.exports = {
-  entry: {
-    historySaver: './src/chrome/historySaver.ts',
-    histories: './src/vue/histories.ts',
-    popup: './src/chrome/popup.ts'
-  },
-
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: '[name].js'
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        loader: 'pug-plain-loader'
-      },
-      {
-        test: /\.scss$/,
-        use: ['vue-style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.vue/,
-        exclude: /node_modules/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-        options: {
-          // transpileOnly: true, // 型チェックしない
-          appendTsSuffixTo: [/\.vue$/],
-          configFile: 'tsconfig.json'
-        }
-      }
-    ]
-  },
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src/'),
-      vue$: 'vue/dist/vue.esm.js'
+module.exports = (env, argv) => {
+  return {
+    entry: {
+      historySaver: './src/chrome/historySaver.ts',
+      histories: './src/vue/histories.ts',
+      popup: './src/chrome/popup.ts'
     },
-    extensions: ['.ts', '.js', '.vue']
-  },
 
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            /* eslint-disable @typescript-eslint/camelcase */
-            drop_console: true // consoleを削除する
-            /* eslint-enable @typescript-eslint/camelcase */
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: '[name].js'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.pug$/,
+          loader: 'pug-plain-loader'
+        },
+        {
+          test: /\.scss$/,
+          use: ['vue-style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.vue/,
+          exclude: /node_modules/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+          options: {
+            // transpileOnly: true, // 型チェックしない
+            appendTsSuffixTo: [/\.vue$/],
+            configFile: 'tsconfig.json'
           }
         }
-      })
-    ]
-  },
+      ]
+    },
 
-  devtool: 'source-map',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
+        vue$: 'vue/dist/vue.esm.js'
+      },
+      extensions: ['.ts', '.js', '.vue']
+    },
 
-  plugins: [new VueLoaderPlugin()]
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              /* eslint-disable @typescript-eslint/camelcase */
+              drop_console: true // consoleを削除する
+              /* eslint-enable @typescript-eslint/camelcase */
+            }
+          }
+        })
+      ]
+    },
+
+    devtool:
+      argv.mode === 'development' ? 'cheap-module-eval-source-map' : 'none',
+
+    plugins: [new VueLoaderPlugin()]
+  };
 };
