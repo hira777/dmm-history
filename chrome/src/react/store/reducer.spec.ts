3b7b784b9efcdb913388e1ba32d4a24d123b56c7
soarflat <soarflat777@gmail.com>
2019-12-26 01:17:23 +0900

    test: Reducer のテストを追加 (#21)

---
+++ chrome/src/react/store/reducer.spec.ts
@@ -0,0 +1,71 @@
+import { getHistories } from '../../../../mock/histories';
+import types from './actionTypes';
+import { reducer, initialState as getInitialState } from './reducer';
+
+describe('reducer', () => {
+  const initialState = getInitialState();
+  const histories = getHistories();
+  test(types.RESTORE_HISTORIES, () => {
+    expect(
+      reducer(initialState, {
+        type: types.RESTORE_HISTORIES,
+        payload: histories
+      })
+    ).toEqual({
+      ...initialState,
+      ...{
+        histories,
+        restoredHistories: true
+      }
+    });
+  });
+
+  test(types.REMOVE_HISTORY, () => {
+    const state = {
+      ...initialState,
+      ...{
+        histories
+      }
+    };
+    const targetId = '12';
+    const expectedHistories = histories.filter(({ id }) => id !== targetId);
+
+    expect(
+      reducer(state, {
+        type: types.REMOVE_HISTORY,
+        payload: targetId
+      })
+    ).toEqual({
+      ...initialState,
+      ...{
+        histories: expectedHistories
+      }
+    });
+  });
+
+  test(types.KEYWORDS_UPDATE, () => {
+    expect(
+      reducer(initialState, {
+        type: types.KEYWORDS_UPDATE,
+        payload: 'すごい'
+      })
+    ).toEqual({
+      ...initialState,
+      ...{
+        keywords: ['すごい']
+      }
+    });
+
+    expect(
+      reducer(initialState, {
+        type: types.KEYWORDS_UPDATE,
+        payload: 'すごい 作品'
+      })
+    ).toEqual({
+      ...initialState,
+      ...{
+        keywords: ['すごい', '作品']
+      }
+    });
+  });
+});
