7ca06f070faf3cd2ea496cb9844f23fb39c166c8
soarflat <soarflat777@gmail.com>
2019-12-30 17:56:33 +0900

    chore: Babel Config に書かれていた @babel/preset-react が何に利用されるのかをコメントを追加 (#21)

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-react',
+    '@babel/preset-react', // Jest でテストするために利用（https://jestjs.io/docs/en/tutorial-react#setup-without-create-react-app）
     '@babel/preset-typescript'
   ]
 };
