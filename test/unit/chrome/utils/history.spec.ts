1dff4507a6e90cbc94b56a56474dbc3e199537a0
soarflat <soarflat777@gmail.com>
2020-08-28 00:51:24 +0900

    fix: ts-jest を利用することで発生した既存テストのエラーを修正した (#24)

--- test/unit/chrome/utils/history.spec.ts
+++ test/unit/chrome/utils/history.spec.ts
@@ -1,30 +1,127 @@
 import { KEYS } from '@/enums';
-import { add, get } from '@/utils/history';
+import { History } from '@/models/history';
+import history from '@/utils/history';
 
 describe('history', () => {
-  const histories = [
-    { id: 12, title: '良い作品' },
-    { id: 25, title: 'すごい作品' },
-    { id: 31, title: '超すごい作品' },
+  const histories: History[] = [
+    {
+      id: '12',
+      title: '良い作品',
+      href: '',
+      imageUrl: '',
+      prices: [100],
+      salePrices: null,
+      saleLimitTime: null,
+    },
+    {
+      id: '25',
+      title: 'すごい作品',
+      href: '',
+      imageUrl: '',
+      prices: [100],
+      salePrices: null,
+      saleLimitTime: null,
+    },
+    {
+      id: '31',
+      title: '超すごい作品',
+      href: '',
+      imageUrl: '',
+      prices: [100],
+      salePrices: null,
+      saleLimitTime: null,
+    },
   ];
 
   describe('add', () => {
     it('新しい履歴を追加する', () => {
-      const newHistory = { id: 88, title: 'とんでもない作品' };
-      expect(add(histories, newHistory)).toEqual([
-        { id: 88, title: 'とんでもない作品' },
-        { id: 12, title: '良い作品' },
-        { id: 25, title: 'すごい作品' },
-        { id: 31, title: '超すごい作品' },
+      const newHistory = {
+        id: '88',
+        title: 'とんでもない作品',
+        href: '',
+        imageUrl: '',
+        prices: [100],
+        salePrices: null,
+        saleLimitTime: null,
+      };
+      expect(history.add(histories, newHistory)).toEqual([
+        {
+          id: '88',
+          title: 'とんでもない作品',
+          href: '',
+          imageUrl: '',
+          prices: [100],
+          salePrices: null,
+          saleLimitTime: null,
+        },
+        {
+          id: '12',
+          title: '良い作品',
+          href: '',
+          imageUrl: '',
+          prices: [100],
+          salePrices: null,
+          saleLimitTime: null,
+        },
+        {
+          id: '25',
+          title: 'すごい作品',
+          href: '',
+          imageUrl: '',
+          prices: [100],
+          salePrices: null,
+          saleLimitTime: null,
+        },
+        {
+          id: '31',
+          title: '超すごい作品',
+          href: '',
+          imageUrl: '',
+          prices: [100],
+          salePrices: null,
+          saleLimitTime: null,
+        },
       ]);
     });
 
     it('新しい履歴のidが既存の履歴idと重複する場合、重複する既存の履歴を削除して新しい履歴を追加する', () => {
-      const newHistory = { id: 25, title: 'すごい作品' };
-      expect(add(histories, newHistory)).toEqual([
-        { id: 25, title: 'すごい作品' },
-        { id: 12, title: '良い作品' },
-        { id: 31, title: '超すごい作品' },
+      const newHistory = {
+        id: '25',
+        title: 'すごい作品',
+        href: '',
+        imageUrl: '',
+        prices: [100],
+        salePrices: null,
+        saleLimitTime: null,
+      };
+      expect(history.add(histories, newHistory)).toEqual([
+        {
+          id: '25',
+          title: 'すごい作品',
+          href: '',
+          imageUrl: '',
+          prices: [100],
+          salePrices: null,
+          saleLimitTime: null,
+        },
+        {
+          id: '12',
+          title: '良い作品',
+          href: '',
+          imageUrl: '',
+          prices: [100],
+          salePrices: null,
+          saleLimitTime: null,
+        },
+        {
+          id: '31',
+          title: '超すごい作品',
+          href: '',
+          imageUrl: '',
+          prices: [100],
+          salePrices: null,
+          saleLimitTime: null,
+        },
       ]);
     });
   });
@@ -36,16 +133,16 @@ describe('history', () => {
           [KEYS.HISTORIES]: histories,
         },
       };
-      expect(get(chromeStorageObject)).toEqual(histories);
+      expect(history.get(chromeStorageObject)).toEqual(histories);
     });
 
     it('履歴が存在しない場合、空の履歴を取得する', () => {
       let chromeStorageObject = {
         [KEYS.DMM_HISTORY]: {},
-      };
-      expect(get(chromeStorageObject)).toEqual([]);
+      } as {};
+      expect(history.get(chromeStorageObject)).toEqual([]);
       chromeStorageObject = {};
-      expect(get(chromeStorageObject)).toEqual([]);
+      expect(history.get(chromeStorageObject)).toEqual([]);
     });
   });
 });
