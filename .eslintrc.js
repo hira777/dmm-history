a3c936fc54e3787e528b1b9c69da34371c27c2a8
soarflat <soarflat777@gmail.com>
2020-08-28 00:40:19 +0900

    chore: React に関連する不要な ESLint のルールをオフにした (#24)

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,8 @@ module.exports = {
   ],
   plugins: ['@typescript-eslint', 'react-hooks'],
   rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off', // 戻り値の推論は TypeScript の型システムに任せたいので off にする
+    'react/prop-types': 'off', // コンパイルした JavaScript を外部から利用されるわけではないので、off にする
     'prettier/prettier': [
       'error',
       {
