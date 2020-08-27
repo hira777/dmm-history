1e4ebdcd6c5c8b3b3fcd0591d1802f1eaa98b1a1
soarflat <soarflat777@gmail.com>
2020-08-28 00:40:50 +0900

    chore: React コンポーネントのテストができるように環境を整えた (#24)

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,13 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/chrome/src/$1',
   },
-  moduleFileExtensions: ['js', 'ts', 'json'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/chrome/tsconfig.json',
+    },
+  },
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
 };
