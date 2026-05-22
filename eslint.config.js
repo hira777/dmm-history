c9ae44f9b1eeb251bf81cc43fdfadfd373846923
soarflat <soarflat777@gmail.com>
2026-05-23 00:02:43 +0900

    chore: 設定ファイルにコメントを追加
    - tsconfigの各オプションに説明を追加
    - ESLint設定の各ブロックに説明を追加

--- eslint.config.js
+++ eslint.config.js
@@ -4,17 +4,25 @@ const eslintConfigPrettier = require('eslint-config-prettier');
 
 module.exports = [
   {
+    // 生成物と依存パッケージはチェック対象から外す
     ignores: ['chrome/build/**', 'dist/**', 'node_modules/**']
   },
+  // JavaScriptの基本的な推奨ルールを使う
   js.configs.recommended,
+  // TypeScript向けの推奨ルールを使う
   ...tseslint.configs.recommended,
+  // Prettierと競合する整形ルールを無効にする
   eslintConfigPrettier,
   {
+    // Chrome拡張のTypeScript/Reactコードをチェックする
     files: ['chrome/src/**/*.{ts,tsx}'],
     languageOptions: {
+      // 最新のJavaScript構文を扱う
       ecmaVersion: 'latest',
+      // import/exportを使う
       sourceType: 'module',
       globals: {
+        // Chrome拡張APIとブラウザのグローバル変数を許可する
         chrome: 'readonly',
         document: 'readonly',
         window: 'readonly',
